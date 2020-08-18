dfd8ec946a452d435284dfd7ba57d341882fcda9
Jeremy Ludwig <hjeremyludwig@gmail.com>
2020-08-18 13:02:34 -0400

    Generate random cards and handle click event

--- app.js
+++ app.js
@@ -2,23 +2,54 @@
 const cardArea = document.querySelector('.cards');
 const newGameBtn = document.querySelector('#new-game');
 
-let numCards = 20;
+let numCards = 10;
 
 
 // Functions
 function startGame() {
     generateCards(numCards);
-    console.log('hello')
 }
 
 function generateCards(num) {
-    for (let i=0; i <= num; i++) {
-        const markup = `
-            <div class='card' id='${i}'></div>
-        `;
-        cardArea.insertAdjacentHTML('beforeend', markup);
+    cardArea.innerHTML = '';
+    let cards = [];
+    // Create cards
+    
+    for(let j = 0; j < 2; j++) {
+        // let subcards = [];
+        for (let i=0; i < num; i++) {
+            const markup = `
+                <div class='card' id='${i}'><img src="assets/card-${i+1}.png"/></div>
+            `;
+            cards.push(markup);
+        };
+    };
+
+
+    let random = randomizeArray(cards);
+
+    random.map( card => {
+        cardArea.insertAdjacentHTML('beforeend', card);
+    })
+}
+
+function randomizeArray(array) {
+    for(let i = array.length - 1; i > 0; i--){
+        const j = Math.floor(Math.random() * i)
+        const temp = array[i]
+        array[i] = array[j]
+        array[j] = temp
     }
+    return array;
+}
+
+function handleClick(e) {
+    if(!e.target.classList.contains('card')) {
+        return;
+    }
+    console.log(e.target.id);
 }
 
 // Event Listeners
+cardArea.addEventListener('click', handleClick);
 newGameBtn.addEventListener('click', startGame);
